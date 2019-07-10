b1437facb9b16cefb1fc29e88980a59dc4993558
Connor Cowan <ccowan003@comcast.net>
2019-07-10 01:46:40 +0000

    Automatically backed up by Learn

--- lib/concerns/paramable.rb
+++ lib/concerns/paramable.rb
@@ -1,6 +1,6 @@
 module Paramable
   module InstanceMethods
-    def to_param              #converts :name to a url friendly parameter
+    def to_param                    #converts :name to a url friendly parameter
       name.downcase.gsub(' ', '-')
     end
   end
