77d541462c6a2138707ddb4865673d52e9ff248b
Connor Cowan <ccowan003@comcast.net>
2019-07-10 01:44:04 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable                     #should define method find_by_name. will be used as a class method
   module ClassMethods
     def find_by_name(name)
-      self.detect{|object| object.name == name}
+      self.detect{|object| object.name}
     end
   end
 end
