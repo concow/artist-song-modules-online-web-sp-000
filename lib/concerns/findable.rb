a5ccdbb0c5a04f4d7e97991a646158b12dd08841
Connor Cowan <ccowan003@comcast.net>
2019-07-10 01:49:42 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Findable                     #should define method find_by_name. will be used as a class method
   module ClassMethods
-    def find_by_name(name)      #self.all vesurs @@class_var
+    def find_by_name(name)      #self.all vs. @@class_var
       self.all.detect{|object| object.name == name}   #detect returns the first item in block that returns true
     end                                               #being our (self) instances
   end
